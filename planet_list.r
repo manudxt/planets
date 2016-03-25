6226c8555fe52efd8d76e7e73d6c098c46946c0e
akl21 <akl21@duke.edu>
2016-03-25 11:43:32 -0400

    Added Pluto

--- planet_list.r
+++ planet_list.r
@@ -2,3 +2,6 @@ planet = c('mercury','venus', 'earth', 'jupiter', 'saturn')
 print(planet)
 print("But what Pluto")
 print("Lets ignore pluto for now.")
+#We found a ninth planet.
+planet = c(planet, "pluto")
+planet
